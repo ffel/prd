7d40b3764aa87ef38e5c0b5d42d9b731409d48e3
Chris van Uffelen <chris.vanuffelen@han.nl>
2016-02-19 14:54:20 +0100

    first attempt for out of prd pkg diagram

---
+++ examples/goingFor.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/ffel/prd"
+)
+
+const (
+	goingForAWalk Proces = iota
+	BobGettingReady
+	AliceGettingReady
+)
+
+const (
+	gotReady Channel = iota
+)
+
+func main() {
+	PrdStart(800, 500)
+
+	LabelChannel(gotReady, "got ready")
+
+	At(0, goingForAWalk).Starts("going for a walk")
+	At(1, goingForAWalk).Creates(AliceGettingReady, "Alice getting ready")
+	At(2, goingForAWalk).Creates(BobGettingReady, "Bob getting ready")
+
+	At(3, goingForAWalk).WantsToReceiveOn(gotReady)
+	At(6, BobGettingReady).WantsToSendOn(gotReady, "true")
+	At(7, goingForAWalk).WantsToReceiveOn(gotReady)
+	At(12, AliceGettingReady).WantsToSendOn(gotReady, "true")
+
+	At(20, goingForAWalk).Terminates()
+
+	// need to fix output of report
+
+	fmt.Fprintln(os.Stderr, PrdEnd().String())
+}
