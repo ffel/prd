66ae32cb832a62b281b5c0225f776a8b4dcefa0e
Chris van Uffelen <chris.vanuffelen@han.nl>
2016-02-20 16:58:15 +0100

    completed going for a walk example

--- examples/goingFor.go
+++ examples/goingFor.go
@@ -11,27 +11,46 @@ const (
 	goingForAWalk Proces = iota
 	BobGettingReady
 	AliceGettingReady
+	armAlarm
+	BobPutsOnShoes
+	AlicePutsOnShoes
 )
 
 const (
 	gotReady Channel = iota
+	putOnShoes
+	armedAlarm
 )
 
 func main() {
 	PrdStart(800, 250)
 
 	LabelChannel(gotReady, "got ready")
+	LabelChannel(armedAlarm, "armed alarm")
+	LabelChannel(putOnShoes, "put on shoes")
 
 	At(0, goingForAWalk).Starts("going for a walk")
-	At(1, goingForAWalk).Creates(AliceGettingReady, "Alice getting ready")
-	At(2, goingForAWalk).Creates(BobGettingReady, "Bob getting ready")
+	At(1, goingForAWalk).Creates(BobGettingReady, "Bob getting ready")
+	At(2, goingForAWalk).Creates(AliceGettingReady, "Alice getting ready")
 
 	At(3, goingForAWalk).WantsToReceiveOn(gotReady)
 	At(6, BobGettingReady).WantsToSendOn(gotReady, "true")
 	At(7, goingForAWalk).WantsToReceiveOn(gotReady)
-	At(12, AliceGettingReady).WantsToSendOn(gotReady, "true")
+	At(9, AliceGettingReady).WantsToSendOn(gotReady, "true")
 
-	At(20, goingForAWalk).Terminates()
+	At(10, goingForAWalk).Creates(armAlarm, "arm alarm")
+	At(11, goingForAWalk).Creates(BobPutsOnShoes, "Bob puts on shoes")
+	At(12, goingForAWalk).Creates(AlicePutsOnShoes, "Alice puts on shoes")
+
+	At(13, goingForAWalk).WantsToReceiveOn(putOnShoes)
+
+	At(16, BobPutsOnShoes).WantsToSendOn(putOnShoes, "true")
+	At(17, goingForAWalk).WantsToReceiveOn(putOnShoes)
+	At(19, AlicePutsOnShoes).WantsToSendOn(putOnShoes, "true")
+
+	At(20, goingForAWalk).WantsToReceiveOn(armedAlarm)
+	At(22, armAlarm).WantsToSendOn(armedAlarm, "true")
+	At(24, goingForAWalk).Terminates()
 
 	PrdEnd()
 
